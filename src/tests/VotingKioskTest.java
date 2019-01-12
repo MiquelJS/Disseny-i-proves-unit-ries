ef48a1d4e0f758fcd0bbeb17d90622a83e3a16bb
MiquelJS <miquel9696@gmail.com>
2019-01-12 22:13:37 +0100

    Tested null votes (with 2 votes)

--- src/tests/VotingKioskTest.java
+++ src/tests/VotingKioskTest.java
@@ -24,6 +24,7 @@ public class VotingKioskTest {
         votingKiosk.vote(votedParty);
         votingKiosk.vote(new Party(""));
         votingKiosk.vote(new Party("null"));
+        votingKiosk.vote(new Party("null"));
         VoteCounter voteCounter = votingKiosk.voteCounter;
 
         // Case when the vote is scrutinized correctly
@@ -36,7 +37,7 @@ public class VotingKioskTest {
         assertEquals(1, voteCounter.getBlanks());
 
         // Case when vote is null
-
+        assertEquals(2,voteCounter.getNulls());
 
         // Case when the vote is on a party that is not in the valid parties
         /*votingKiosk.vote(anotherParty);
